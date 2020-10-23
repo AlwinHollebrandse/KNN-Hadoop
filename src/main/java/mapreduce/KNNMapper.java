a9e973745f92739daf02229e0a349022e4e11534
Alwin Hollebrandse <hollebrandsaj@mymail.vcu.edu>
2020-10-23 15:08:46 -0400

    mapper 1.1

--- src/main/java/mapreduce/KNNMapper.java
+++ src/main/java/mapreduce/KNNMapper.java
@@ -1,5 +1,59 @@
 package mapreduce;
 
-public class KNNMapper {
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class KNNMapper extends Mapper<Object, Text, Text, IntWritable> {
+
+}
+	
+	private Text itemset = new Text();
+	
+	public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+		
+		String transaction = value.toString();
+		
+		List<String> itemsets = getItemSets(transaction.split("\n"));
+		
+		for(String item : itemsets) {
+			itemset.set(item);
+			context.write(itemset, new IntWritable(1));
+		}	
+	}
+	
+	//getting powersets (excluding empty set)
+	private List<String> getItemSets(String[] items) {
+				
+		List<String> itemsets = new ArrayList<String>();	
+				
+		int n = items.length;
+				
+		int[] masks = new int[n];
+				
+		for (int i = 0; i < n; i++)
+			masks[i] = (1 << i);
+				
+		for (int i = 0; i < (1 << n); i++){				
+					
+			List<String> newList = new ArrayList<String>(n);
+					
+			for (int j = 0; j < n; j++){
+			
+				if ((masks[j] & i) != 0){      	
+					newList.add(items[j]);
+		        }
+		                
+		        if(j == n-1 && newList.size() > 0 && newList.size() < 5){
+		        	itemsets.add(newList.toString());
+		        }
+			}
+		}
+		        			
+		return itemsets;
+	}
 }
--- src/main/java/mapreduce/WordCount.java
+++ src/main/java/mapreduce/WordCount.java
@@ -3,11 +3,15 @@ package mapreduce;
 // PATH=$PATH:$HOME/hadoop-3.3.0/bin
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.ArrayWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,46 +19,164 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
 public class WordCount {
+	
+	public static class DoubleArrayWritable extends ArrayWritable {
 
-	public static class TokenizerMapper extends Mapper<Object, Text, Text, IntWritable>{
+	    public DoubleArrayWritable(DoubleWritable[] values) {
+	        super(DoubleWritable.class, values);
+	    }
 
-		private final static IntWritable one = new IntWritable(1);
+	    @Override
+	    public DoubleWritable[] get() {
+	        return (DoubleWritable[]) super.get();
+	    }
+
+	    @Override
+	    public String toString() {
+	        DoubleWritable[] values = get();
+	        return values[0].toString() + ", " + values[1].toString();
+	    }
+	}
+
+	public static class TokenizerMapper extends Mapper<Object, Text, Text, DoubleArrayWritable>{
+		private final static DoubleWritable instanceClass = new DoubleWritable();
+		private final static DoubleWritable instanceDistance = new DoubleWritable();
+
+		private List<String> allTestInstances = new LinkedList<String>();
 		private Text word = new Text();
+		
+		protected void setup(Context context) throws IOException, InterruptedException {
+	        Configuration conf = context.getConfiguration();
+	        for(String testInstance : conf.get("testInstances").split("\n")) {
+	        	allTestInstances.add(testInstance);
+	        }
+	    }
 
 		public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
-			StringTokenizer itr = new StringTokenizer(value.toString());
-			while (itr.hasMoreTokens()) {
-				word.set(itr.nextToken());
-				context.write(word, one);
+//			List<String> allTestInstances = Arrays.asList(value.toString().split("\n"));
+			List<String> allTrainInstances = Arrays.asList(value.toString().split("\n"));
+			
+			for (int test = 0; test < allTestInstances.size(); test++) { // TODO go through train?
+				
+				List<String> testInstance = Arrays.asList(allTestInstances.get(test).split(","));
+				
+				for (int train = 0; test < allTrainInstances.size(); train++) {
+
+					List<String> trainInstance = Arrays.asList(allTrainInstances.get(train).split(","));
+
+			        // calc distance
+					double distance = 0;
+			        for (int i = 0; i < trainInstance.size() - 1; i++) {
+						double diff = Double.parseDouble(testInstance.get(i)) - Double.parseDouble(trainInstance.get(i));
+						distance += diff * diff;
+					}
+			        
+			        instanceClass.set(Double.parseDouble(trainInstance.get(trainInstance.size() - 1)));
+			        instanceDistance.set(Math.sqrt(distance));
+			        
+			        DoubleWritable[] temp = new DoubleWritable[2];
+			        DoubleArrayWritable output = new DoubleArrayWritable(temp);
+
+			        temp[0] = instanceClass;
+			        temp[1] = instanceDistance;
+			        output.set(temp);
+			        
+			        word.set(Integer.toString(test)); // "temp" // TODO all share same key? or is each key the test index
+					context.write(word, new DoubleArrayWritable(output.get()));	
+				}
+//				word.set("temp"); // TODO all share same key?
+//				context.write(word, new DoubleArrayWritable(output.get()));	
 			}
 		}
 	}
 
-	public static class IntSumReducer extends Reducer<Text,IntWritable,Text,IntWritable> {
-		private IntWritable result = new IntWritable();
+	public static class IntSumReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable> {
+		private DoubleWritable result = new DoubleWritable();
+		private double[][] classDistanceMatrix;
+		private int testInstancesLength;
 
-		public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+		protected void setup(Context context) throws IOException, InterruptedException {
+	        Configuration conf = context.getConfiguration();
+	        testInstancesLength = Integer.parseInt(conf.get("testInstancesLength"));
+	        int k = Integer.parseInt(conf.get("k"));
+	        classDistanceMatrix = new double[testInstancesLength][k * 2];
+	    }
+		
+		public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException {
+			for (int test = 0; test < testInstancesLength; test++) { // TODO go through train?
+				// TODO rn im assuming that map has output sorted by distance and not class or something
+				
+			}
+			
 			int sum = 0;
-			for (IntWritable val : values) {
+			for (DoubleWritable val : values) {
 				sum += val.get();
 			}
 			result.set(sum);
 			context.write(key, result);
 		}
+		
+//		protected void cleanup(Context context) throws IOException, InterruptedException {
+//			// Performs majority voting using the first k first elements of an array
+//			int kVoting(int k, float** shortestKDistances) {
+//			    map<float, int> classCounter;
+//			    for (int i = 0; i < k; i++) {
+//			        classCounter[shortestKDistances[i][1]]++;
+//			    }
+//
+//			    int voteResult = -1;
+//			    int numberOfVotes = -1;
+//			    for (auto i : classCounter) {
+//			        if (i.second > numberOfVotes) {
+//			            numberOfVotes = i.second;
+//			            voteResult = i.first;
+//			        }
+//			    }
+//			    return voteResult;
+//			}
+//		}
 	}
 
 	public static void main(String[] args) throws Exception {
+		if (args.length != 4) { // TODO add check that instances of train and test are same length
+	      System.out.printf("Usage: KNN <input dir that holds train data> <test data file> <output dir> <k>\n");
+	      System.exit(-1);
+	    }
+		
 		Configuration conf = new Configuration();
+		String testInstacesString = getTestInstances(args[1]);
+	    conf.set("testInstances", testInstacesString);
+        String[] allTestInstancesTemp = testInstacesString.split("\n");
+        conf.set("testInstancesLength",  Integer.toString(allTestInstancesTemp.length));
+	    conf.set("k", args[3]);
 		Job job = Job.getInstance(conf, "word count");
 		job.setJarByClass(WordCount.class);
 		job.setMapperClass(TokenizerMapper.class);
 		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
 		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		FileOutputFormat.setOutputPath(job, new Path(args[2]));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
+	
+	public static String getTestInstances(String testFile) throws FileNotFoundException {
+		File myObj = new File(testFile);
+		Scanner myReader = new Scanner(myObj);
+		StringBuilder result = new StringBuilder();
+		while (myReader.hasNextLine()) {
+			result.append(myReader.nextLine());
+		}
+		myReader.close();
+		return result.toString();
+	}
+	
+	// https://stackoverflow.com/questions/25432598/what-is-the-mapper-of-reducer-setup-used-for/25450627
+	// https://stackoverflow.com/questions/28914596/mapreduce-output-arraywritable
 }
